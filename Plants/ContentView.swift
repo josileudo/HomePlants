ef113c8f11b8e629d8f0fba7ec218ba9552c6c17
Josileudo <josileudorodrigues75@gmail.com>
2022-11-12 11:55:25 -0300

    :tada: init: Initial commit

---
+++ Plants/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plants
+//
+//  Created by Josileudo Rodrigues on 12/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Plants/PlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsApp.swift
+//  Plants
+//
+//  Created by Josileudo Rodrigues on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

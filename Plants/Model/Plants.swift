eb10620fc901877dca3a510844e6051522064a95
Josileudo <josileudorodrigues75@gmail.com>
2022-11-12 12:15:39 -0300

    :label: type: Create the model plants

---
+++ Plants/Model/Plants.swift
@@ -0,0 +1,23 @@
+//
+//  Plants.swift
+//  Plants
+//
+//  Created by Josileudo Rodrigues on 12/11/22.
+//
+
+import SwiftUI
+
+// MARK: Plants model
+struct Plant: Identifiable {
+    var id: UUID = .init();
+    var imageName: String;
+    var title: String;
+    var price: String;
+}
+
+var Plants: [Plant] = [
+    .init(imageName: "asplenium", title: "Asplenium Nidus", price: "$56.70"),
+    .init(imageName: "crassula", title: "Crassula Ovata", price: "$38.35"),
+    .init(imageName: "nephrolepis", title: "nephrolepis", price: "$106.54"),
+    .init(imageName: "peperomia", title: "peperomia", price: "$87.99")
+]
